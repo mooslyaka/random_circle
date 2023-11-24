f08268a829bd9fb938e30cbe61d527ecd6142772
mooslyaka <mooslyakaboowamp@gmail.com>
2023-11-24 12:06:51 +0300

    first

--- main.py
+++ main.py
@@ -2,35 +2,43 @@ import sys
 from random import randint
 from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
 
-class QT(QMainWindow):
+
+class Example(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)  # Загружаем дизайн
-        self.pushButton.clicked.connect(self.run)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(500, 500, 500, 500)
+        self.btn = QPushButton('ok', self)
+        self.btn.setGeometry(200, 200, 50, 50)
         self.do_paint = False
+        self.btn.clicked.connect(self.paint)
 
     def paintEvent(self, event):
         if self.do_paint:
             qp = QPainter()
             qp.begin(self)
-            self.draw(qp)
+            self.draw_flag(qp)
             qp.end()
         self.do_paint = False
 
-    def draw(self, qp):
-        zxc = randint(10, 500)
-        x = randint(0, 800)
-        y = randint(0, 600)
-        qp.setBrush(QColor(255, 255, 0))
-        qp.drawEllipse(x, y, zxc, zxc)
-    def run(self):
+    def paint(self):
         self.do_paint = True
         self.update()
 
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+        zxc = randint(10, 400)
+        x = randint(0, 500)
+        y = randint(0, 500)
+        qp.drawEllipse(x, y, zxc, zxc)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = QT()
+    ex = Example()
     ex.show()
     sys.exit(app.exec())
